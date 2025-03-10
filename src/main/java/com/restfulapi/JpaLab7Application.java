0bd61c69c48bd3329a54ab949261dc0d3f829b06
umar-alii <umarrali.pk@gmail.com>
2025-03-11 03:22:34 +0500

    n

---
+++ src/main/java/com/restfulapi/JpaLab7Application.java
@@ -0,0 +1,13 @@
+package com.restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLab7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLab7Application.class, args);
+    }
+
+}
